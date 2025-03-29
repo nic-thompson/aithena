bdabcd0718ec0dd53c34ece26d6be45ff949e287
Nicolas Thompson <mail@nicolasthompson.com>
2025-03-29 17:01:51 +0000

    fix: finalize static files config for production

--- aithena/settings.py
+++ aithena/settings.py
@@ -33,6 +33,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'aithena.urls'
